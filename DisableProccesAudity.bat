ca0eb9e82437db834505ca62666704c3b04bac32
GeneralAnasazi <jan_it@gmx.net>
2018-11-28 16:04:50 +0100

    - include auto start feature (will be start when Crossout is started)

---
+++ DisableProccesAudity.bat
@@ -0,0 +1,3 @@
+@REM Disable Audit Process logging (needed to start the translater with Crossout)
+@REM This Batch needs Administrator Rights
+auditpol.exe /set /subcategory:{0CCE922B-69AE-11D9-BED3-505054503030} /success:disable
