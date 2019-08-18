0dbc6f597f1fa7fbc03ffec09ce64c2763467fbc
GeneralAnasazi <jan_it@gmx.net>
2019-08-18 14:20:14 +0200

    bugfix autostart

--- DisableProccesAudity.bat
+++ DisableProccesAudity.bat
@@ -1,3 +1,4 @@
 @REM Disable Audit Process logging (needed to start the translater with Crossout)
 @REM This Batch needs Administrator Rights
 auditpol.exe /set /subcategory:{0CCE922B-69AE-11D9-BED3-505054503030} /success:disable
+schtasks /Delete /TN "AutostartCrossoutTranslator" /F
--- EnableProccesAudity.bat
+++ EnableProccesAudity.bat
@@ -1,3 +1,4 @@
 @REM Enable Audit Process logging (needed to start the translater with Crossout)
 @REM This Batch needs Administrator Rights
 auditpol.exe /set /subcategory:{0CCE922B-69AE-11D9-BED3-505054503030} /success:enable
+schtasks /Create /XML "C:\Projekte\Delphi\CrossoutTranslater\Win32\Release\AutostartCrossoutTranslator.xml" /TN "AutostartCrossoutTranslator"
