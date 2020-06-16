67f3398db703f820321ee14cb4a54ea4f1a9b9fe
GeneralAnasazi <jan_it@gmx.net>
2020-06-16 19:37:22 +0200

    - Add Function Export Result to Clipboard

--- EnableProccesAudity.bat
+++ EnableProccesAudity.bat
@@ -1,4 +1,4 @@
 @REM Enable Audit Process logging (needed to start the translater with Crossout)
 @REM This Batch needs Administrator Rights
 auditpol.exe /set /subcategory:{0CCE922B-69AE-11D9-BED3-505054503030} /success:enable
-schtasks /Create /XML "E:\Projekte\Delphi\XO Translater\AutostartCrossoutTranslator.xml" /TN "AutostartCrossoutTranslator"
+schtasks /Create /XML "AutostartCrossoutTranslator.xml" /TN "AutostartCrossoutTranslator"
